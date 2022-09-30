bd5d727896ca49a561fdbf569396751d57eca9bd
Zahid <k21043022@nmslab-004151.nms.kcl.ac.uk>
2022-09-30 14:17:07 +0100

    Updated solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,21 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+
+    for item in items:
+
+        if item in frequencies:
+            frequencies[item] += 1
+
+        else:
+            frequencies[item] = 1
+            
+    
     return frequencies
+
+
+
+
+
+frequencies([50,50, 60, 70, 100, 100, 100, 30])
